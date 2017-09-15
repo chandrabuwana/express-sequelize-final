981fac0c814ee4e6260434eac538627102973822
chandrabuwana <cbuwana@gmail.com>
2017-09-15 11:32:13 +0700

    blm smua

--- livecode4/routes/supplier.js
+++ livecode4/routes/supplier.js
@@ -4,55 +4,55 @@ let model=require('../models')
 
 router.get('/',(req,res)=>{
   model.Supplier.findAll()
-  .then(function(supplier){
+  .then(function(data){
     res.render('supplier',{data:data})
   })
 })
 
-// router.get('/add/:id',(req,res)=>{
-//   res.render('supplier-add',{err:false})
-// })
-//
-// router.post('/add',(req,res)=>{
-//   model.Supplier.create({
-//     name:req.body.name,
-//     kota:req.body.kota
-//   })
-//   .then(()=>{
-//     res.redirect('/supplier')
-//   })
-// })
-//
-// router.get('/edit/:id',(req,res)=>{
-//   model.Supplier.findById(req.params.id)
-//   .then((supplier)=>{
-//     res.render('supplier-edit',{Datasupplier:supplier})
-//   })
-// })
-//
-// router.post('/edit:id',(req,res)=>{
-//   model.Supplier.update({
-//     name:req.body.name,
-//     kota:req.body.kota
-//   },{
-//     where:{
-//       id:req.params.id
-//     }
-//   })
-//   .then(()=>{
-//     res.redirect('/dokter')
-//   })
-// })
-//
-// router.get('/delete/:id',(req,res)=>{
-//   model.Supplier.destroy({
-//     where:{
-//       id:req.params.id
-//     }
-//   })
-//   .then(()=>{
-//     res.redirect('/supplier')
-//   })
-// })
+router.get('/add',(req,res)=>{
+  res.render('supplier-add',{err:false})
+})
+
+router.post('/add',(req,res)=>{
+  model.Supplier.create({
+    name:req.body.name,
+    kota:req.body.kota
+  })
+  .then(()=>{
+    res.redirect('/supplier')
+  })
+})
+
+router.get('/edit/:id',(req,res)=>{
+  model.Supplier.findById(req.params.id)
+  .then((supplier)=>{
+    res.render('supplier-edit',{Datasupplier:supplier})
+  })
+})
+
+router.post('/edit:id',(req,res)=>{
+  model.Supplier.update({
+    name:req.body.name,
+    kota:req.body.kota
+  },{
+    where:{
+      id:req.params.id
+    }
+  })
+  .then(()=>{
+    res.redirect('/supplier')
+  })
+})
+
+router.get('/delete/:id',(req,res)=>{
+  model.Supplier.destroy({
+    where:{
+      id:req.params.id
+    }
+  })
+  .then(()=>{
+    res.redirect('/supplier')
+  })
+})
 
 module.exports=router
